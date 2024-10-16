b7c110ee80e095f9ad49c3311d06727b05fe2fa9
cirin0 <ivan.gruziv@gmail.com>
2024-10-16 21:49:57 +0300

    Refactor app.py and add new ref templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, abort
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -29,6 +29,29 @@ def admin():
     to_url = url_for("greeting", name='administrator', age=30, _external=True)
     print(to_url)
     return redirect(to_url)
+
+
+posts = [
+    {"id": 1, 'title': 'My First Post',
+        'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day',
+        'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2',
+        'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+
+@app.route('/posts')
+def get_posts():
+    return render_template("posts.html", posts=posts)
+
+
+@app.route('/post/<int:id>')
+def get_post(id):
+    if id > len(posts):
+        abort(404)
+    post = posts[id-1]
+    return render_template('detail-post.html', post=post)
 
 
 if __name__ == '__main__':
