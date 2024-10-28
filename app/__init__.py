3c28cc2dd6c406b6c6290215e93af2e2fa07ec35
cirin0 <ivan.gruziv@gmail.com>
2024-10-28 15:31:00 +0200

    ref and add Blueprint for post

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_pyfile('../config.py')
+
+from . import views  # noqa: E402, F401
+from app.posts import post_bp  # noqa: E402, F401
+app.register_blueprint(post_bp)
---
+++ app/views.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, redirect, url_for, abort
+from . import app
+
+
+@app.route('/')
+def main():
+    return render_template('hello.html')
+
+
+@app.route('/homepage')
+def home():
+    """View foe the Home page of your website"""
+    agent = request.user_agent
+
+    return render_template('home.html', agent=agent)
+
+
+@app.route('/hi/<string:name>')
+def greeting(name):
+    name = name.upper()
+    age = request.args.get('age', 0, int)
+    return render_template('hi.html', name=name, age=age)
+
+
+@app.route('/admin')
+def admin():
+    to_url = url_for("greeting", name='administrator', age=30, _external=True)
+    print(to_url)
+    return redirect(to_url)
+
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title='Моє резюме')
