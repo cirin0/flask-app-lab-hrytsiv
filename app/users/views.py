23b9e35dd5c13aceeee588b02cd443307b244e3f
cirin0 <ivan.gruziv@gmail.com>
2024-11-06 12:40:09 +0200

    add cookie

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, redirect, url_for
+from flask import make_response, render_template, request, redirect, url_for
 from . import user_bp
+from datetime import timedelta
 
 
 @user_bp.route('/hi/<string:name>')
@@ -15,3 +16,26 @@ def admin():
                      name='administrator', external=True)
     print(to_url)
     return redirect(to_url)
+
+
+@user_bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука установлена')
+    response.set_cookie('username', 'student',
+                        max_age=timedelta(seconds=60).total_seconds())
+    response.set_cookie('color', 'black',
+                        max_age=timedelta(seconds=60).total_seconds())
+    return response
+
+
+@user_bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+
+@user_bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0)
+    return response
